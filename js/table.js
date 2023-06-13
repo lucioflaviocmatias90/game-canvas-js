d3f1305bfef8c1c5d757f18c4bb169d46a5a25bd
Lucio Flavio C. Matias <lucioflavio.cmatias90@hotmail.com>
2023-06-13 16:06:37 -0300

    create function to make table

--- js/table.js
+++ js/table.js
@@ -1,35 +1,39 @@
-const table = document.querySelector("table");
+function createTable(condition) {
+  if (condition) {
+    const table = document.querySelector("table");
 
-collisionsMap.forEach((row) => {
-  const tr = document.createElement("tr");
+    collisionsMap.forEach((row) => {
+      const tr = document.createElement("tr");
 
-  row.forEach((column) => {
-    const td = document.createElement("td");
+      row.forEach((column) => {
+        const td = document.createElement("td");
 
-    td.innerHTML = column;
-    td.style.width = "30px";
-    td.style.height = "30px";
+        td.innerHTML = column;
+        td.style.width = "30px";
+        td.style.height = "30px";
 
-    switch (column) {
-      case 27:
-        td.style.backgroundColor = "rgba(0, 0, 255, 0.2)";
-        break;
+        switch (column) {
+          case 27:
+            td.style.backgroundColor = "rgba(0, 0, 255, 0.2)";
+            break;
 
-      case 39:
-        td.style.backgroundColor = "rgba(0, 255, 0, 0.2)";
-        break;
+          case 39:
+            td.style.backgroundColor = "rgba(0, 255, 0, 0.2)";
+            break;
 
-      case 1025:
-        td.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
+          case 1025:
+            td.style.backgroundColor = "rgba(255, 0, 0, 0.2)";
 
-      default:
-        break;
-    }
+          default:
+            break;
+        }
 
-    tr.appendChild(td);
-  });
+        tr.appendChild(td);
+      });
 
-  table.appendChild(tr);
-});
+      table.appendChild(tr);
+    });
+  }
+}
 
-console.log(table);
+createTable(false);
